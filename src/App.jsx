a2a581010d8a02c4dd1e7ccebfd50d562e1fef1e
nemiko007 <otoha.youjopero2@gmail.com>
2023-12-06 20:30:31 +0900

    sumahotaiou

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 export default function App() {
   return (
     <>
-      <nav class="navbar is-fixed-top is-primary is-mobile">
+      <nav class="navbar is-fixed-top is-primary">
         <div class="navbar-menu">
           <div class="navbar-start">
             <a class="navbar-item" href="#works">Works</a>
