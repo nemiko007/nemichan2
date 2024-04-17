ef3136f88807403058751ef80c18a7b9f69b7504
nemiko007 <otoha.youjopero2@gmail.com>
2024-04-17 11:42:32 +0900

    profile_update

--- src/compornents/About.jsx
+++ src/compornents/About.jsx
@@ -13,7 +13,7 @@ export default function About() {
                 </h2>
                 <img src="/nemiko.jpg" />
                 <p>
-                    日本大学文理学部地理学科 ３年
+                    日本大学文理学部地理学科 ４年
                 </p>
                 <p>
                     専攻分野：リモートセンシング
@@ -22,6 +22,18 @@ export default function About() {
                     趣味：音楽とサブカルチャー
                 </p>
                 <br></br>
+                <p>
+                    資格
+                </p>
+                <p>
+                    普通自動車第一種運転免許
+                </p>
+                <p>
+                    中学校教諭第一種免許状（理科）　取得見込み
+                </p>
+                <p>
+                    高等学校教諭第一種免許状（理科）　取得見込み
+                </p>
                 <a href="https://twitter.com/nemiko007"><i class="fa-brands fa-x-twitter fa-5x"></i></a>
             </div>
         </>
